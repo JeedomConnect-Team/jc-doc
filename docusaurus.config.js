3ae2876b13fee7dabaa10f7c657f0a9f20ce9962
jared-94 <stephane.vento@gmail.com>
2023-04-11 06:00:04 +0200

    editUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,7 +44,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/jared-94/jc-doc/edit/main/docs/",
+            "https://github.com/jared-94/jc-doc/edit/main/",
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
