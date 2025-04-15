a9dda7f249bf96f147ca0bd65bb861821c8a5ee7
ToMtOm <tomtom.infos@gmail.com>
2025-04-15 11:27:16 +0200

    ADD title in showcase

--- src/pages/showcase.js
+++ src/pages/showcase.js
@@ -1,5 +1,8 @@
 import Layout from '@theme/Layout';
 import React from 'react';
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import styles from './index.module.css';
 
 const Showcase = () => {
 
@@ -13,6 +16,27 @@ const Showcase = () => {
         <Layout
             title="Gallerie"
         >
+            <header className={clsx('hero hero--primary', styles.heroBanner)}>
+                <div className="container">
+                    <h1 className="hero__title">Créations en pagaille</h1>
+                    <p>
+                        Voici quelques illustrations d'<b>exemples</b> provenant de différentes installations,<br />
+                        où l'on peut voir que chaque utilisateur à la <b>liberté d'exprimer sa créativité</b> <br />
+                        en personnalisant son interface selon ses <b>besoins et envies</b>.
+                    </p>
+                    <div className={styles.buttons}>
+                        <Link
+                            className={clsx(
+                                'button button--outline button--secondary button--md',
+                                styles.doc,
+                            )}
+                            href="https://community.jeedom.com/t/montrez-vos-creations-sur-lappli/58184">
+                            Voir d'autres exemples
+                        </Link>
+                    </div>
+                </div>
+            </header>
+
             <div style={{ justifyContent: 'center', alignItems: 'center', alignSelf: 'center', marginTop: 15, width: '70%' }} >
                 <img src='img/gallery/JeedomConnect_screenshot1.png' width='200px' />
                 <img src='img/gallery/JeedomConnect_screenshot2.png' width='200px' />
